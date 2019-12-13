a870a847d7ab811a5003c16ff790adc252c75cf4
Jon McClure <jmac@Jons-MacBook-Pro.local>
2019-12-13 17:23:04 -0500

    hom

--- packages/guide/pages/components/sections/home/index.js
+++ packages/guide/pages/components/sections/home/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Reuters from 'Components/common/icons/Reuters';
+import Ring from 'Components/common/icons/Ring';
 import classnames from 'classnames';
 import { component } from './styles.scss';
 
 const Home = (props) => (
-  <div className={classnames(component)}>
-    <h5>Home</h5>
+  <div className={classnames(component, 'markdown-default')}>
+    <div className='jumbo'>
+      <div className='container brand'>
+        <div className='team-logo'>
+          <Ring width='30' /><Reuters width='125' /> Graphics
+        </div>
+      </div>
+      <h1>Style.</h1>
+    </div>
   </div>
 );
 
